ba1d86ad605c41718094e55b5b463fcb8155ed7e
PilarPinto <piapintoch@unal.edu.co>
2020-01-06 21:05:01 +0000

    [Add] - Adding the prototypes

--- monty.h
+++ monty.h
@@ -30,7 +30,11 @@ typedef struct stack_s
 } stack_t;
 
 /**
- *
+ * struct aux_s - Global representation
+ * @val: integer
+ * @file: points to the previous element of the stack (or queue)
+ * @line_inf: points to the next element of the stack (or queue)
+ * @flag_sq: 0 or 1 isf is a queue
  *
  */
 typedef struct aux_s
@@ -41,6 +45,7 @@ typedef struct aux_s
 	int flag_sq;
 } aux_t;
 
+/*Global definition*/
 extern aux_t aux;
 
   /**
@@ -60,12 +65,38 @@ typedef struct instruction_s
 
 
 /*Functions*/
-/*son 21*/
-void (*f)(stack_t **stack, unsigned int line_number)
-;void (*f)(stack_t **stack, unsigned int line_number);
-void (*f)(stack_t **stack, unsigned int line_number);
-void (*f)(stack_t **stack, unsigned int line_numbe
-void (*f)(stack_t **stack, unsigned int line_number);
+/*main*/
+void freePlates(stack_t *stack);
+int opcSelec(char *line_inf, stack_t **stack, unsigned int lnum, FILE *file);
+/*maths*/
+void addm(stack_t **stack, unsigned int line_num);
+void subm(stack_t **stack, unsigned int line_num);
+void mulm(stack_t **stack, unsigned int line_num);
+void divm(stack_t **stack, unsigned int line_num);
+void modm(stack_t **stack, unsigned int line_num);
 
+
+/*actions*/
+void push_data(stack_t **stack, unsigned int line_number);
+void add_top(stack_t **stack, unsigned int line_number);
+void add_last(stack_t **stack, unsigned int line_number);
+void delete_top(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+
+
+/*display*/
+void print_stack(stack_t **stack, unsigned int line_number);
+void print_top(stack_t **stack, unsigned int line_number);
+void printTopC(stack_t **stack, unsigned int line_number);
+void printTopS(stack_t **stack, unsigned int line_number);
+
+/*modifiers*/
+void swap2top(stack_t **stack, unsigned int line_number);
+void rotate_top(stack_t **stack, unsigned int line_number);
+void rotate_bottom(stack_t **stack, unsigned int line_number);
+
+/*data structures*/
+void queuef(stack_t **stack, unsigned int line_number);
+void stackf(stack_t **stack, unsigned int line_number);
 
 #endif
