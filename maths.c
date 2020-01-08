0a8258f5533e2200d30886fe193df5995938adb5
PilarPinto <piapintoch@unal.edu.co>
2020-01-08 15:13:36 +0000

    [Fix]- Correct issues of compilation

--- maths.c
+++ maths.c
@@ -1,8 +1,9 @@
 #include "monty.h"
 
 /**
- *
- *
+ * addm - Function to add
+ * @stack: Is the pointer to the stack mem address
+ * @line_num: Is the actual line number
  */
 void addm(stack_t **stack, unsigned int line_num)
 {
@@ -12,14 +13,14 @@ void addm(stack_t **stack, unsigned int line_num)
 	pl_node = *stack;
 	while (pl_node != NULL)
 	{
-		pl_node = pld_node->next;
+		pl_node = pl_node->next;
 		len++;
 	}
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_num);
-		fclose(aux.file);
-		free(aux.line_inf);
+		fclose(global.f_hand);
+		free(global.monty_line);
 		freePlates(*stack);
 		exit(EXIT_FAILURE);
 	}
@@ -31,10 +32,11 @@ void addm(stack_t **stack, unsigned int line_num)
 }
 
 /**
- *
- *
+ * subm - Function to substract
+ * @stack: Is the pointer to the stack mem address
+ * @line_num: Is the actual line number
  */
-void subm(stack_t **stack, unsigned int line_number)
+void subm(stack_t **stack, unsigned int line_num)
 {
 	stack_t *pl_node;
 	int sub, len = 0;
@@ -42,14 +44,14 @@ void subm(stack_t **stack, unsigned int line_number)
 	pl_node = *stack;
 	while (pl_node != NULL)
 	{
-		pl_node = pld_node->next;
+		pl_node = pl_node->next;
 		len++;
 	}
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_num);
-		fclose(aux.file);
-		free(aux.line_inf);
+		fclose(global.f_hand);
+		free(global.monty_line);
 		freePlates(*stack);
 		exit(EXIT_FAILURE);
 	}
@@ -61,12 +63,12 @@ void subm(stack_t **stack, unsigned int line_number)
 
 }
 
-
 /**
- *
- *
+ * mulm - Function to multiply
+ * @stack: Is the pointer to the stack mem address
+ * @line_num: Is the actual line number
  */
-void mulm(stack_t **stack, unsigned int line_number)
+void mulm(stack_t **stack, unsigned int line_num)
 {
 	stack_t *pl_node;
 	int mul, len = 0;
@@ -74,14 +76,14 @@ void mulm(stack_t **stack, unsigned int line_number)
 	pl_node = *stack;
 	while (pl_node != NULL)
 	{
-		pl_node = pld_node->next;
+		pl_node = pl_node->next;
 		len++;
 	}
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_num);
-		fclose(aux.file);
-		free(aux.line_inf);
+		fclose(global.f_hand);
+		free(global.monty_line);
 		freePlates(*stack);
 		exit(EXIT_FAILURE);
 	}
@@ -95,10 +97,11 @@ void mulm(stack_t **stack, unsigned int line_number)
 
 
 /**
- *
- *
+ * divm - Function to divide
+ * @stack: Is the pointer to the stack mem address
+ * @line_num: Is the actual line number
  */
-void divm(stack_t **stack, unsigned int line_number)
+void divm(stack_t **stack, unsigned int line_num)
 {
 	stack_t *pl_node;
 	int div, len = 0;
@@ -106,23 +109,23 @@ void divm(stack_t **stack, unsigned int line_number)
 	pl_node = *stack;
 	while (pl_node != NULL)
 	{
-		pl_node = pld_node->next;
+		pl_node = pl_node->next;
 		len++;
 	}
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_num);
-		fclose(aux.file);
-		free(aux.line_inf);
+		fclose(global.f_hand);
+		free(global.monty_line);
 		freePlates(*stack);
 		exit(EXIT_FAILURE);
 	}
 	pl_node = *stack;
-	if(pl_node->n == 0)
+	if (pl_node->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_num);
-		fclose(aux.file);
-		free(aux.line_inf);
+		fclose(global.f_hand);
+		free(global.monty_line);
 		freePlates(*stack);
 		exit(EXIT_FAILURE);
 
@@ -134,12 +137,12 @@ void divm(stack_t **stack, unsigned int line_number)
 
 }
 
-
 /**
- *
- *
+ * modm - Function to find the module
+ * @stack: Is the pointer to the stack mem address
+ * @line_num: Is the actual line number
  */
-void modm(stack_t **stack, unsigned int line_number)
+void modm(stack_t **stack, unsigned int line_num)
 {
 stack_t *pl_node;
 	int div, len = 0;
@@ -147,23 +150,23 @@ stack_t *pl_node;
 	pl_node = *stack;
 	while (pl_node != NULL)
 	{
-		pl_node = pld_node->next;
+		pl_node = pl_node->next;
 		len++;
 	}
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_num);
-		fclose(aux.file);
-		free(aux.line_inf);
+		fclose(global.f_hand);
+		free(global.monty_line);
 		freePlates(*stack);
 		exit(EXIT_FAILURE);
 	}
 	pl_node = *stack;
-	if(pl_node->n == 0)
+	if (pl_node->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_num);
-		fclose(aux.file);
-		free(aux.line_inf);
+		fclose(global.f_hand);
+		free(global.monty_line);
 		freePlates(*stack);
 		exit(EXIT_FAILURE);
 
