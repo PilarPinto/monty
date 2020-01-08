b5afd7920b5aac27947ab1d45725422685219f18
PilarPinto <piapintoch@unal.edu.co>
2020-01-08 15:12:52 +0000

    [Fix]- Correct issues of compilation

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "monty.h"
+global_t global;
+
 /**
  * main - Is the main function
  * @argc:Is the index of argv
@@ -22,7 +24,7 @@ int main(int argc, char **argv)
 	}
 
 	file_handle = fopen(argv[1], "r");
-	aux.file = file_handle;
+	global.f_hand = file_handle;
 
 	if (file_handle == NULL)
 	{
@@ -34,31 +36,31 @@ int main(int argc, char **argv)
 	{
 		line_inf = NULL;
 		line_content = getline(&line_inf, &size, file_handle);
-		aux.line_inf = line_inf;
-		line_number++;
+		global.monty_line = line_inf;
+		lnum++;
 
 		if (line_content > 0)
 		{
-			opcSelec(line_inf, &stack, lnum, file);
+			opcSelec(line_inf, &stack, lnum, file_handle);
 		}
 		free(line_inf);
 	}
 	freePlates(stack);
-	fclose(file);
+	fclose(file_handle);
 
-return(0);
+return (0);
 }
 
 
 /**
- * freePlates
+ * freePlates - Release the mem use of the stack elem
  * @stack: Pointer to stack mem address
  */
 void freePlates(stack_t *stack)
 {
-	stack_t *plate;
-	plate = stack;
-	while(stack)
+	stack_t *plate = stack;
+
+	while (stack)
 	{
 		plate = stack->next;
 		free(stack);
@@ -70,44 +72,47 @@ void freePlates(stack_t *stack)
 /**
  * opcSelec - Selector of functions
  * @line_inf: Is the words in the line
- * @stack: Pointer to the stack mem add
+ * @stck: Pointer to the stack mem add
  * @lnum: current liner number
- * @file: Is the monty file
+ * @f_handle: Is the monty file
+ * Return: 0 if is compared opcode is found 1 is not
  */
-int opcSelec(char *line_inf, stack_t **stack, unsigned int lnum, FILE *file)
+int opcSelec(char *line_inf, stack_t **stck, unsigned int lnum, FILE *f_handle)
 {
-	unsigned int ind  = 0;
+	unsigned int ind = 0;
 	char *codeOp;
 
 	instruction_t listOpc[] = {
 		{"push", push_data}, {"pall", print_stack}, {"pint", print_top},
-		{"pop", delete_top}, {"swap", swap2top}, {"add", addm},
-		{"nop", nop}, {"sub", subm}, {"div", divm}, {"mul", mulm},
+		{"pop", delete_top}, {"swap", swap2top}, {"nop", nop},
+		{"add", addm}, {"sub", subm}, {"div", divm}, {"mul", mulm},
 		{"mod", modm}, {"pchar", printTopC}, {"pstr", printTopS},
-		{"rotl", rotateTop}, {"rotr", rotateBottom}, {"queue", queuef},
-		{"stack", stackf}, {NULL, NULL}
+		{"rotl", rotate_top}, {"rotr", rotate_bottom},
+		{"queue", queuef}, {"stack", stackf}, {NULL, NULL}
 	};
+
 	codeOp = strtok(line_inf, " \n\t");
-	if(codeOp && listOpc[0] == "#")
+
+	if (codeOp && codeOp[0] == '#')
 		return (0);
 
-	aux.val = strtok(NULL, " \n\t");
+	global.p_data = strtok(NULL, " \n\t");
 
 	while (listOpc[ind].opcode && codeOp)
 	{
 		if (strcmp(codeOp, listOpc[ind].opcode) == 0)
 		{
-			listOpc[ind].f(stack, lnum);
+			listOpc[ind].f(stck, lnum);
 			return (0);
 		}
-		i++;
+		ind++;
 	}
 	if (codeOp && listOpc[ind].opcode == NULL)
 	{
-		fpritnf(stderr, "L%d: unknown instruction %s\n", lnum, codeOp);
-		fclose(file);
+		fprintf(stderr, "L%d: unknown instruction %s\n", lnum, codeOp);
+		fclose(f_handle);
 		free(line_inf);
-		free_nodes(*stack);
+		freePlates(*stck);
 		exit(EXIT_FAILURE);
 	}
 	return (1);
